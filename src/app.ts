7fae0ee0b41466588b0723d44a5647a9cb9c4c73
Sean <swbrooks87@gmail.com>
2020-09-15 21:19:02 -0400

    Add config package and twit functions

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 const path = require('path');
+import twitter from './controllers/twitter';
 
 const app = express();
 const port = 3000;
@@ -11,11 +12,11 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req: any, res: Response) => {
-    console.log(__dirname + '/index.html');
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', (req: any, res: any) => {
+    let data = twitter();
+    return res.status(200).json(data);
 });
 
-app.listen(process.env.PORT, '0.0.0.0', () => {
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
--- src/controllers/twitter.ts
+++ src/controllers/twitter.ts
@@ -1,4 +1,34 @@
-export const twitterApi = () => {
-    console.log("Twitter api pull for hashtag");
-}
-setInterval(twitterApi, 10000);
+"use strict";
+
+const client = require('twit');
+const config = require('config');
+
+let process = () => {
+    let date = new Date();
+    let T = new client({
+        consumer_key:         config.get('twitter.key'),
+        consumer_secret:      config.get('twitter.secret'),
+        access_token:         config.get('twitter.token'),
+        access_token_secret:  config.get('twitter.token_secret'),
+        timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+        strictSSL:            true,     // optional - requires SSL certificates to be valid.
+      });
+      
+    T.get('search/tweets', { q: `#everythingseanb`, count: 5 }, function(err : any, data : any, response : any) {
+        for (let i = 0; i < data['statuses'].length; i++) {
+            let tweet_id : string = data['statuses'][i]['id_str'];
+            T.post('statuses/retweet/:id', { id: tweet_id }, function(err : any, data : any, response : any) {
+                console.log(err);
+            });
+        }
+    });
+      
+};
+
+export default process;
+ 
+// const twitterApi = () => {
+//     console.log("Twitter api pull for hashtag");
+// }
+// setInterval(twitterApi, 10000);
+
