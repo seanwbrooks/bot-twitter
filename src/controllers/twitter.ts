4a27e1ed6cc33687fba1e25c54ceeddd0318b17d
Sean <swbrooks87@gmail.com>
2020-09-28 18:53:28 -0400

    Twitter work and disable live

--- src/controllers/twitter.ts
+++ src/controllers/twitter.ts
@@ -5,8 +5,34 @@ import { IError } from '../models/error';
 import { isAudience } from '../utils/audience';
 import { query } from '../utils/timers';
 
-let twitterApi = () => {
-    let T = new client({
+const twitterApi = () => {
+    const T = connect();
+
+    const hour = new Date().getUTCHours();
+    console.log(query(hour));
+    const topic = query(hour);
+    
+    const tweets : any = getTweets(topic, T);
+
+    if (tweets['statuses']) {
+        for (let i = 0; i < tweets['statuses'].length; i++) {
+            if (!tweets['statuses'][i].favorited) {
+                T.post('favorites/create', { id: tweets['statuses'][i].id_str }, function(err : any, data : any, response : any) {
+                    console.log(data);
+                });
+            }
+        }
+    }
+}
+
+const getTweets = (query : string, T : any) => {
+    T.get('search/tweets', { q: query, count: 10 }, function(err : Array<IError>, data : any, response : any) {
+        console.log(data);
+    });
+}
+
+const connect = () => {
+    return new client({
         consumer_key:         process.env.KEY,
         consumer_secret:      process.env.SECRET,
         access_token:         process.env.TOKEN,
@@ -14,20 +40,6 @@ let twitterApi = () => {
         timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
         strictSSL:            true,     // optional - requires SSL certificates to be valid.
       });
-
-    const hour = new Date().getUTCHours();
-
-    T.get('search/tweets', { q: query(hour), count: 10 }, function(err : Array<IError>, data : any, response : any) {
-        if (data['statuses']) {
-            for (let i = 0; i < data['statuses'].length; i++) {
-                if (!data['statuses'][i].favorited) {
-                    T.post('favorites/create', { id: data['statuses'][i].id_str }, function(err : any, data : any, response : any) {
-                        console.log(data);
-                    });
-                }
-            }
-        }
-    });
 }
 
 
