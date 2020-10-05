f8d16e4fed77fe0a98dd2f110ca473df57d9be75
Sean <swbrooks87@gmail.com>
2020-10-05 12:02:24 -0400

    Worker update

--- src/clock.ts
+++ src/clock.ts
@@ -1,7 +1,7 @@
 var cron = require('node-cron');
-var worker = require('./worker');
+var worker = require('./worker.js');
  
 cron.schedule('* * * * *', () => {
   console.log('running a task every minute');
-  worker;
+  worker();
 });
