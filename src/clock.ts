30cab3d2aace5daee1c81a26621610855783492e
Sean <swbrooks87@gmail.com>
2020-10-05 12:12:02 -0400

    add twitter call

--- src/clock.ts
+++ src/clock.ts
@@ -1,7 +1,9 @@
 var cron = require('node-cron');
-import { worker } from './worker';
+import { worker } from './worker.js';
+import twitter from './controllers/twitter';
  
 cron.schedule('* * * * *', () => {
   console.log('running a task every minute');
   worker;
+  twitter;
 });
