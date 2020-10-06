4a70285f90dd06e79ab25d9b1bc64e65d4c171cd
Sean <swbrooks87@gmail.com>
2020-10-05 20:15:32 -0400

    query update

--- src/controllers/twitter.ts
+++ src/controllers/twitter.ts
@@ -17,11 +17,10 @@ const twitterApi = () => {
           });
     
         const hour = new Date().getUTCHours();
-        console.log(hour);
         const topic = query(hour);
-        
-        console.log("seaching for " + topic);
-        T.get('search/tweets', { q: topic, count: 100, language: 'en' }, function(err : Array<IError>, data : any) {
+
+        T.get('search/tweets', { q: ['real estate', 'financial independence', 'javascript', 'personal finance', 'code newbie'], count: 100, language: 'en' }, function(err : Array<IError>, data : any) {
+            console.log(data);
             if (data['statuses']) {
                 for (let i = 0; i < data['statuses'].length; i++) {
                     if (!data['statuses'][i].possibly_sensitive) {
