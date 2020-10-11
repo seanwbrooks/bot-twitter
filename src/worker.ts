851bb8e7ad6b221a428e5259f724b6015916ab5a
Sean <swbrooks87@gmail.com>
2020-10-11 13:09:57 -0400

    New content

--- src/worker.ts
+++ src/worker.ts
@@ -12,34 +12,40 @@ const worker = () => {
         strictSSL:            true,     // optional - requires SSL certificates to be valid.
       });
 
-    const index =  Math.floor(Math.random() * 10); 
+    const index =  Math.floor(Math.random() * 14); 
 
     var img = tweets[index].img["file"];
     console.log(img);
-      
-    var b64content = fs.readFileSync(`./img/${img}`, { encoding: 'base64' });
- 
-    // first we must post the media to Twitter
-    T.post('media/upload', { media_data: b64content }, function (err : any, data: any) {
-        // now we can assign alt text to the media, for use by screen readers and
-        // other text-based presentations and interpreters
-        var mediaIdStr = data.media_id_string;
-        var altText = tweets[index].img["alt"];
-        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
-        console.log("media uploaded");
-        console.log(data);
-        
-        T.post('media/metadata/create', meta_params, function (err : any, data : any) {
-            if (!err) {
-                // now we can reference the media and post a tweet (media will attach to the tweet)
-                var params = { status: tweets[index].text, media_ids: [mediaIdStr] };
+    
+    if (img) {
+        var b64content = fs.readFileSync(`./img/${img}`, { encoding: 'base64' });
+     
+        // first we must post the media to Twitter
+        T.post('media/upload', { media_data: b64content }, function (err : any, data: any) {
+            // now we can assign alt text to the media, for use by screen readers and
+            // other text-based presentations and interpreters
+            var mediaIdStr = data.media_id_string;
+            var altText = tweets[index].img["alt"];
+            var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+            console.log("media uploaded");
+            console.log(data);
             
-                T.post('statuses/update', params, function (err : any, data : any) {
-                    console.log(data)
-                });
-            }
+            T.post('media/metadata/create', meta_params, function (err : any, data : any) {
+                if (!err) {
+                    // now we can reference the media and post a tweet (media will attach to the tweet)
+                    var params = { status: tweets[index].text, media_ids: [mediaIdStr] };
+                
+                    T.post('statuses/update', params, function (err : any, data : any) {
+                        console.log(data)
+                    });
+                }
+            });
         });
-    })
+    } else {
+        T.post('statuses/update', { status: tweets[index].text }, function (err : any, data : any) {
+            console.log(data)
+        });
+    }
 }
 
 export default worker;
@@ -65,17 +71,17 @@ const tweets =
         `increase your productivity, and keep you living a long and ` +
         `happy life.\r\n My favorite garlic, lemon stuffed trout!\r\n ` +
         `What's your go to meal?\r\n` +
-        `#healthyliving #productivity #health #recovery`,
+        `#healthyliving #health #recovery`,
         "img": {
             "file": "homecooking.jpg",
             "alt": "Good looking guy smiling with a sheet of stuffed trout."
         }
     },
     {
-        "text": `Most important trait to being successful is being proactive!\r\n` +
-        `7 Habits of Highly Effective People by @StephenRCovey is a great read for leaders and managers.\r\n` +
-        `Don't let things happen to you, be the thing that happens to the world!\r\n` +
-        `#highlyeffective #leadership #productivity #goals #success`,
+        "text": `Most important trait to being successful is being proactive!\r\n\n` +
+        `7 Habits of Highly Effective People by @StephenRCovey is a great read for leaders and managers.\r\n\n` +
+        `Don't let things happen to you, be the thing that happens to the world!\r\n\n` +
+        `#leadership #productivity #goals #success`,
         "img": {
             "file": "",
             "alt": ""
@@ -120,29 +126,77 @@ const tweets =
         }
     },
     {
-        "text": "Have a lovely day!\r\n #lovelyday #blessed #thankful #gratitude #happy",
+        "text": "Have a lovely day!\r\n\n" +
+        "Today is your day.\r\n\n" +
+        "#lovelyday #blessed #thankful #gratitude #happy",
         "img": {
             "file": "",
             "alt": ""
         }
     },
     {
-        "text": "You matter, so pay yourself first.\r\n  I do this by automatically pulling 30% of my paycheck every month" +
+        "text": "You matter, so pay yourself first.\r\n\nI do this by automatically pulling 30% of my paycheck every month" +
         " and putting 5% into a high yield savings account and 25% in a brokerage account that invests in mutual " +
-        "funds.\r\n Start here: https://investor.vanguard.com/home\r\n" +
-        "#investing #moneytips #mutualfunds @Vanguard #payyourselffirst",
+        "funds.\r\n\nStart here: https://investor.vanguard.com/home\r\n\n" +
+        "#invest #moneytips @Vanguard",
         "img": {
             "file": "",
             "alt": ""
         }
     },
     {
-        "text": "Is there a habit that you do that keeps you excited about life?\r\n" +
-        "I'm thinking about having a habit of jumping in the ocean each month...\r\n" +
+        "text": "Is there a habit that you do that keeps you excited about life?\r\n\n" +
+        "I'm thinking about having a habit of jumping in the ocean each month...\r\n\n" +
         "#productivity #ocean #swimming #habits #atomichabits #refreshing",
         "img": {
             "file": "oceanswimming.MOV",
             "alt": "Sean Brooks jumping into the ocean."
+        }
+    },
+    {
+        "text": "You should know where every dollar goes.\r\n\n" +
+        "When I receive any income, gift, or money, I know that 20% will go to " +
+        "my savings account, 30% into my brokerage account, 20% will pay any expenses I have," +
+        " and 30% will be spent on anything I want.\r\n\n" +
+        "#moneytips #investing #debt #budget2020",
+        "img": {
+            "file": "",
+            "alt": ""
+        }
+    },
+    {
+        "text": "How to hit $100K+ net worth?\r\n\n" +
+        "1) Follow your intuition and passions\r\n" +
+        "2) Self-educate and read daily\r\n" +
+        "3) Surround yourself with ppl who love and support you\r\n" +
+        "4) Live frugally\r\n" +
+        "5) Think long term\r\n" +
+        "#fire #financialfreedom #financialindependence #moneytips #moneyadvice #selfimprovement",
+        "img": {
+            "file": "networth.jpg",
+            "alt": "Mint summary net worth $153,367."
+        }
+    },
+    {
+        "text": "Rules that Warren Buffett live by:\r\n\n" +
+        "1) Never lose money\r\n" +
+        "2) Never forget Rule #1\r\n\n" +
+        "What do you think?\r\n\n" +
+        "#moneytips #investing #growthmindset #financialindependence #patience" + 
+        "#rules #system @WarrenBuffett #moneyadvice #money",
+        "img": {
+            "file": "",
+            "alt": ""
+        }
+    },
+    {
+        "text": "You are not alone thinking you are your biggest critic.\r\n\n" +
+        "Everyone struggles. It is important to break our negative scripts.\r\n\n" +
+        `Be patient with yourself and give yourself credit.\r\n\n` +
+        "#selflove #selfhelp #growthmindset #goalsetting #goals #selfcare",
+        "img": {
+            "file": "",
+            "alt": ""
         }
     }
 ];
